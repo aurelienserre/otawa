major bug fix in covariance handling
- if statement was inverse of what it was supposed to be (the `not` was
  not supposed to be here)
- the parameter was called `cov` but the var `covariance` was used in
  the code, so covariance provided as param could not be used

otawa/base.py
@@ -16,7 +16,7 @@ class BaseCost(abc.ABC):
         pass
 
 
-def log_likelihood_gaussian(diff, cov=None):
+def log_likelihood_gaussian(diff, covariance=None):
     """Compute log likelihood with gaussian model, from diff.
 
     diff = y_hat - y_true
@@ -27,7 +27,7 @@ def log_likelihood_gaussian(diff, cov=None):
     # cf. 1st comment on https://stats.stackexchange.com/a/30466/237874
     # this brings singularity problems, and det(Sigma) < 0 which doesn't work in
     # the log_likelihood term -1/2 log(det(Sigma))
-    if cov is not None:
+    if covariance is None:
         # if no covariance provided, estimates it on `diff`
         covariance = np.diag(np.var(diff, axis=0, ddof=1))
     # slower implementation